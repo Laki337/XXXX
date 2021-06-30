407823b50d933c17f5f7939563680a9ece32a8af
BuildTools <unconfigured@null.spigotmc.org>
2021-06-30 23:14:37 +0200

    primerchik

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	String xr= "askhdbasdadas1d53as1d35as1d35as1d35as1d3a5sd15as";
+            char[] r = xr.toCharArray();
+
+            for(int i=0; i< r.length; i++) {
+                if(r[i] > 48 && r[i]){
+
+
+
+                }
+
+            }
+    }
+}
